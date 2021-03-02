a9d1029ad024176ef9d00ea1034bdf0b1d38aeee
sumin03 <breath.ing0210@gmail.com>
2021-03-03 03:02:37 +0900

    Class: Component life cycle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0
   };
@@ -11,8 +15,18 @@ class App extends React.Component{
   };
   minus = () => {
     this.setState(current => ({count: current.count -1}));
+  };  
+  componentDidMount(){
+    console.log("component rendered");
   }
-;  render(){
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+  componentWillUnmount(){
+    console.log("Goodbye, cruel WoRlD");
+  }
+  render(){
+    console.log("I am rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
