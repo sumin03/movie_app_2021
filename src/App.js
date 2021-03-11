a5b3a041fae2460744b25dad18b3b0424b7a241e
sumin03 <breath.ing0210@gmail.com>
2021-03-12 03:28:24 +0900

    Class: Deploying to Github Pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
-
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    //It is free to add state! It is up to me
     isLoading: true,
-    movies: []
+    movies: [],
   };
-  getMovies = async() => {
-    const {data:{data :{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading : false});
-  }
-  componentDidMount(){
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({
+      movies,
+      isLoading: false,
+    });
+  };
+  componentDidMount() {
     this.getMovies();
   }
-  render(){
-    const {isLoading, movies} = this.state; 
-    return <div>{isLoading ? "Loading..." : movies.map(movie => {
-      console.log(movie);
-      return <Movie id={movie.id} key={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-    })}</div>;
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {" "}
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text"> Loading... </span>
+          </div>
+        ) : (
+          <div className="movies">
+            {" "}
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
